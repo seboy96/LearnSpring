a2a8445268a20bc17ad24ff0697f566208843d98
Sean <seboy96@gmail.com>
2020-04-28 20:30:39 -0400

    .

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/Student.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/Student.java
@@ -8,7 +8,11 @@ public class Student {
 	private String lastName;
 	private String country;
 	
+	private String favoriteLanguage;
+	
 	private LinkedHashMap<String, String> countryOptions;
+	
+	private String[] operatingSystems;
 	
 	public Student() {
 		// populate country options: used ISO country code
@@ -49,7 +53,22 @@ public class Student {
 	public LinkedHashMap<String, String> getCountryOptions() {
 		return countryOptions;
 	}
-	
+
+	public String getFavoriteLanguage() {
+		return favoriteLanguage;
+	}
+
+	public void setFavoriteLanguage(String favoriteLanguage) {
+		this.favoriteLanguage = favoriteLanguage;
+	}
+
+	public String[] getOperatingSystems() {
+		return operatingSystems;
+	}
+
+	public void setOperatingSystems(String[] operatingSystems) {
+		this.operatingSystems = operatingSystems;
+	}
 	
 	
 }
